f819e232d79eb90919d901d7ed84a5088602b2f8
Marta Bakešová <marta.bakesova@centrum.cz>
2024-04-07 22:31:26 +0200

    temer hotovo

---
+++ spolecne.js
@@ -0,0 +1,7 @@
+const tlacitko = document.querySelector("#menu-tlacitko");
+
+tlacitko.addEventListener("click", () => {
+  const rozbal = document.querySelector("#menu-polozky");
+  rozbal.classList.toggle("show");
+    
+});
